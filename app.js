057d049ccedee85e979f37afad49e09bdf25b1d9
ramya_ravindranath <ramya.ravindranathp@gmail.com>
2015-07-06 22:03:51 +1000

    comments added

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
 // MODULE
 var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
 
-// ROUTES
+// rourtes
 weatherApp.config(function ($routeProvider) {
    
     $routeProvider
     
     .when('/', {
         templateUrl: 'home.htm',
-        controller: 'homeController'
+        controller: 'homectrl' // routed when '/' is encountered
     })
     
     .when('/forecast', {
-        templateUrl: 'forecast.htm',
-        controller: 'forecastController'
+        templateUrl: 'forecast.htm',  // routed when '/forecast is encountered
+        controller: 'forecastctrl'
     })
     
 });
-// services
+// customed services
 weatherApp.service('cityService',function()
 {
 	this.city="Melbourne,AU";
 });
 
 
-
-// CONTROLLERS
-weatherApp.controller('homeController', ['$scope','cityService',function($scope,cityService) {
+// controllers
+weatherApp.controller('homectrl', ['$scope','cityService',function($scope,cityService) {
     $scope.city=cityService.city;
 	$scope.$watch('city',function()
 	{
@@ -36,8 +35,9 @@ weatherApp.controller('homeController', ['$scope','cityService',function($scope,
 	});
 }]);
 
-weatherApp.controller('forecastController', ['$scope','$resource','cityService', function($scope,$resource,cityService) {
+weatherApp.controller('forecastctrl', ['$scope','$resource','cityService', function($scope,$resource,cityService) {
     $scope.city=cityService.city;
+    // fetching data from api openweatherapp
     $scope.weatherAPI=$resource("http://api.openweathermap.org/data/2.5/forecast/daily",{  
     callback:"JSON_CALLBACK"}, {get:{method:"JSONP"}});  
     $scope.weatherResult=$scope.weatherAPI.get({q:$scope.city, cnt:7});
